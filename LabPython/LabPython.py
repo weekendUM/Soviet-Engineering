d9d886e2fae4f04c4ac63925ec6b10817e1f352d
Schronk <damian.sirbu01@e-uvt>
2020-11-17 17:50:54 +0200

    reset for binary thing

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -11,15 +11,15 @@ import base_conversion as bc
 
 #x = int(input("Numar: "))
 print("おはよう世界　Good morning World!")
-a = fraction(-1)
-test = polynomial(1,-6,9)
+#a = fraction(-1)
+#test = polynomial(1,-6,9)
 #print(test.calc_value(fraction(1)))
-for i in test.calc_roots():
-    print(i,end=' ')
+#for i in test.calc_roots():
+    #print(i,end=' ')
 
 #print(a**7)
 
-#print(bc.to_binary(12))
+print(bc.to_binary(12))
 
 
 #print(prime.prim(138857))
