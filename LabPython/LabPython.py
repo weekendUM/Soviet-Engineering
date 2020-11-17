a58011cadd3d73503e3ddc775cd8f634c14d7a88
Schronk <damian.sirbu01@e-uvt>
2020-11-17 17:48:24 +0200

    optimized the power function

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,7 +1,8 @@
 #import test_class
 import __future__
 #from fractions import fraction
-#from polynomial import polynomial
+from polynomial import polynomial
+from fractions import fraction
 #import prime
 #import medie_note
 #import dictionary1
@@ -10,13 +11,15 @@ import base_conversion as bc
 
 #x = int(input("Numar: "))
 print("おはよう世界　Good morning World!")
-#test = polynomial(1,-6,9)
+a = fraction(-1)
+test = polynomial(1,-6,9)
 #print(test.calc_value(fraction(1)))
-#for i in test.calc_roots():
-#    print(i,end=' ')
-#print()
+for i in test.calc_roots():
+    print(i,end=' ')
 
-print(bc.to_binary(12))
+#print(a**7)
+
+#print(bc.to_binary(12))
 
 
 #print(prime.prim(138857))
