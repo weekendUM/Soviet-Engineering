e5058fc0abf4a5e20bccc82962584683ea347c0d
Schronk <damian.sirbu01@e-uvt>
2020-12-16 11:52:30 +0200

    not sure what this commit is for

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,30 +1,18 @@
-class item:
-    name = 'blank'
-    price = 0
+class copil:
+    UUID= '1234AV'
+    name= 'Grigore Rasputin'
+    class adresa:
+        Country= 'Romania'
+        City= 'Bucuresti'
+        street= 'lala'
+        number= '241'
+    cadouri=[]
+class cadou:
+    def init(self, obiect, cantitate, adjective, UID):
+        self.obiect= obiect
+        self.cantitate= cantitate
+        self.adjective= adjective
+        self.UID= UID
 
-    def __init__(self, name, price):
-        print("constructor for item class called...")
-        self.name = name
-        self.price = price
 
-    def __hash__(self):
-        print("hashing item...")
-        return str(name + str(price)).__hash__()
-
-class cart():
-    items = {}
-    
-    def append(self, new_item : str, amount = 1):
-        print("adding item...")
-        self.items[new_item] = amount
-
-    def __getitem__(self, item_name : str):
-        print("showing item...")
-        return self.items[item_name]
-
-cart1 = cart()
-cart1.append("suc", 3)
-cart1.append("mere", 5)
-cart1.append("paine")
-
-print(cart1["mere"])
+test = cadou('a')
---
+++ LabPython/cart.py
@@ -0,0 +1,23 @@
+class item:
+    name = 'blank'
+    price = 0
+
+    def __init__(self, name, price):
+        print("constructor for item class called...")
+        self.name = name
+        self.price = price
+
+    def __hash__(self):
+        print("hashing item...")
+        return str(name + str(price)).__hash__()
+
+class cart():
+    items = {}
+    
+    def append(self, new_item : str, amount = 1):
+        #print("adding item...")
+        self.items[new_item] = amount
+
+    def __getitem__(self, item_name : str):
+        #print("showing item...")
+        return self.items[item_name]
