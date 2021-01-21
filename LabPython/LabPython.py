88a850ca346c7088ba0b8e39f86654e5aea34b7e
Schronk <damian.sirbu01@e-uvt>
2021-01-21 15:24:55 +0200

    asd memoization editing distance

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,34 +1,64 @@
-from multiprocessing import Process
-from datetime import datetime as time
+class Planta():
+    def __init__(self, nume, clima, info):
+        self.nume = nume
+        self.clima = clima
+        self.info = info
+    def __str__(self):
+        rez = "clima: " + self.clima + "\nnume: " + self.nume + "\ninfo:" + self.info
+        return rez
+    
+    def __repr__(self):
+        rez = "clima: " + self.clima + "\nnume: " + self.nume + "\ninfo:" + self.info
+        return rez
+    
+class Trandafir(Planta):
+    def __init__(self, clima, info, culoare, inaltime):
+        super().__init__("trandafir", clima, info)
+        self.culoare = culoare
+        self.inaltime = inaltime
+    def __str__(self):
+        rez = Planta.__str__(self)
+        rez += "\nculoare: " + self.culoare + "\ninaltime: " + str(self.inaltime)
+        
+class Conifer(Planta):
+    def __init__(self, nume, clima, info, inaltime):
+        super().__init__(nume, clima, info)
+        self.inaltime = inaltime
 
-def test(start : int, end : int):
-    for i in range(start, end):
-        j=0
-        for i in range(100):
-            j += 1
+idk = []
+idk.append(Planta("lalea", "temperat", "..."))
+idk.append(Trandafir("temperat", " ...", "rosu", 20))
+idk.append(Trandafir("temperat", "... ", "albastru", 10))
+idk.append(Trandafir("temperat", "... ", "rosu", 30))
+idk.append(Conifer("pin", "tundra", "...", 10))
+#print(idk)
 
-if __name__ == '__main__':
-    #print("hello world")
+plante = {}
+for i in idk:
+    if i.clima in plante:
+        plante[i.clima].append(i.nume)
+    else:
+        plante[i.clima] = [i.nume]
+for i in plante:
+    print(i, ":\n")
+    for j in plante[i]:
+        print(j)
+    print()
 
-    no_processes = int(input("Insert the amount of processes you want to start(this should be the number of actual, non-virtual threads, your CPU has): "))
-    size = 1000000000
-    processes = []
+trandafiri = []
+for i in idk:
+    if type(i) == Trandafir:
+        trandafiri.append(i)
 
-    start = time.now()
-    print(f"starting {no_processes} processes...")
-    ops = size // no_processes
-    l_start = 0
-    for i in range(no_processes - 1):
-        l_stop = l_start + ops
-        processes.append(Process(target = test, args = (l_start, l_stop,)))
-        l_start = l_start + ops
-    processes.append(Process(target = test, args = (l_start, size,)))
+culori = {}
+for i in trandafiri:
+    print(i.culoare)
+    if i.culoare in culori:
+        plante[i.culoare].append(i.inaltime)
+    else:
+        plante[i.culoare] = [i.inaltime]
 
-    for p in processes:
-        p.start()
-
-    for p in processes:
-        p.join()
-
-    print(f"time elapsed with {no_processes} process(es) for {size} steps: ", time.now() - start)
-
+for i in culori:
+    print(i)
+    for j in culori[i]:
+        print(j)
