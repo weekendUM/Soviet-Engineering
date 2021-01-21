39472bf830c8cf8ca93faccdaeb50643aeb17127
Schronk <damian.sirbu01@e-uvt>
2021-01-21 15:25:43 +0200

    ...

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,64 +1,86 @@
-class Planta():
-    def __init__(self, nume, clima, info):
-        self.nume = nume
-        self.clima = clima
-        self.info = info
-    def __str__(self):
-        rez = "clima: " + self.clima + "\nnume: " + self.nume + "\ninfo:" + self.info
-        return rez
-    
-    def __repr__(self):
-        rez = "clima: " + self.clima + "\nnume: " + self.nume + "\ninfo:" + self.info
-        return rez
-    
-class Trandafir(Planta):
-    def __init__(self, clima, info, culoare, inaltime):
-        super().__init__("trandafir", clima, info)
-        self.culoare = culoare
-        self.inaltime = inaltime
-    def __str__(self):
-        rez = Planta.__str__(self)
-        rez += "\nculoare: " + self.culoare + "\ninaltime: " + str(self.inaltime)
-        
-class Conifer(Planta):
-    def __init__(self, nume, clima, info, inaltime):
-        super().__init__(nume, clima, info)
-        self.inaltime = inaltime
+import os
 
-idk = []
-idk.append(Planta("lalea", "temperat", "..."))
-idk.append(Trandafir("temperat", " ...", "rosu", 20))
-idk.append(Trandafir("temperat", "... ", "albastru", 10))
-idk.append(Trandafir("temperat", "... ", "rosu", 30))
-idk.append(Conifer("pin", "tundra", "...", 10))
-#print(idk)
+word1 = 'caiet'
+word2 = 'cort'
 
-plante = {}
-for i in idk:
-    if i.clima in plante:
-        plante[i.clima].append(i.nume)
-    else:
-        plante[i.clima] = [i.nume]
-for i in plante:
-    print(i, ":\n")
-    for j in plante[i]:
-        print(j)
-    print()
+dis = []
 
-trandafiri = []
-for i in idk:
-    if type(i) == Trandafir:
-        trandafiri.append(i)
+ex = -1
 
-culori = {}
-for i in trandafiri:
-    print(i.culoare)
-    if i.culoare in culori:
-        plante[i.culoare].append(i.inaltime)
-    else:
-        plante[i.culoare] = [i.inaltime]
+for i in range(len(word1)):
+    buffer = []
+    for j in range(len(word2)):
+        buffer.append(0)
+    dis.append(buffer)
 
-for i in culori:
-    print(i)
-    for j in culori[i]:
-        print(j)
+if(word1[0]==word2[0]):
+    dis[0][0]=0
+else:
+    dis[0][0] = 1
+
+def calc_dist(i, j):
+    global ex
+    ex += 1
+    if(i==j and i==0):
+        return dis[0][0]
+    if(j==0):
+        dis[i][j] = i
+        return dis[i][j]
+    if(i==0):
+        dis[i][j] = j
+        return dis[i][j]
+    if(word1[i]==word2[j]):
+        if(dis[i][j]!=0):
+            return dis[i][j]
+        else:
+            dis[i][j] = calc_dist(i-1,j-1)
+            return dis[i][j]
+    if(word1[i]!=word2[j]):
+        if(dis[i][j]!=0):
+            return dis[i][j]
+        else:
+            dis[i][j] = min(calc_dist(i-1,j-1),calc_dist(i,j-1),calc_dist(i-1,j)) + 1
+            return dis[i][j]
+
+
+
+try:
+    print("Distanta de editare cu memoizare:", calc_dist(len(word1) - 1,len(word2) - 1))
+    print("Apeluri de functii recursive efectuate cu memoizare:", ex)
+    ex = -1
+    for l in dis:
+        print(l)
+except:
+    print('failed')
+
+try:
+    cls = int(input("Doriti sa executam programul si fara memoizare?(raspundeti cu 1 pt adevarat)\nRaspuns: "))
+except:
+    os._exit(0)
+
+if(cls == 1):
+    print('Incepem fara memoizare')
+    def calc_disti(i, j):
+        global ex
+        ex += 1
+        if(i==j and i==0):
+            if(word1[0]==word2[0]):
+                return 0
+            else:
+                return 1
+        if(j==0):
+            return i
+        if(i==0):
+            return j
+        if(word1[i]==word2[j]):
+            return calc_disti(i-1,j-1)
+        if(word1[i]!=word2[j]):
+            return min(calc_disti(i-1,j-1),calc_disti(i,j-1),calc_disti(i-1,j)) + 1
+
+    try:
+        print("Distanta de editare fara memoizare:", calc_disti(len(word1) - 1,len(word2) - 1))
+        print("Apeluri de functii recursive efectuate fara memoizare:", ex)
+    except:
+        print('failed')
+else:
+    pass
