0e5175c070ccccc54213db69f9963b53b8cda5cb
Schronk <damian.sirbu01@e-uvt>
2021-01-26 09:21:44 +0200

    matrix checkpoint

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,70 +1,40 @@
-a= [3,1,5,10]
-b= [4,2,6,1]
-c= [5,3,1,7]
-op = 0
+from math import sqrt
 
-def n3():
-    global op
-    rez = False
-    for i in a:
-        for j in b:
-            for k in c:
-                op += 1
-                if(i==j==k):
-                    rez = True
-    return rez
+A = [
+    [2, 3, 5],
+    [1, 1, 4],
+    [2, 2, 6],
+    [3, 3, 1]
+    ]
 
+e_norm = []
 
+cursor = 0
 
-def n2():
-    global op
-    rez =  False
-    for i in a:
-        for j in b:
-            op += 1
-            if(i==j):
-                for k in c:
-                    op += 1
-                    if(i==k):
-                        rez = True
-                        break
-    return rez
+def mergesort(arr):
+    global cursor
+    if(len(arr) == 2):
+        if(arr[0] > arr[1]):
+            temp = arr[0]
+            arr[0] = arr[1]
+            arr[1] = temp
+            return arr
+        return arr
+    else:
+        arr[:len(arr)//2] = mergesort(arr[:len(arr)//2])
+        cursor = len(arr)//2
+        arr[len(arr)//2:] = mergesort(arr[len(arr)//2:])
+        buffer = []
+        i = 0
+        j = 0
+        #while()
+        return arr
+    pass
 
-def maxnm(m : int):
-    global op
-    rez = False
-    n = len(a)
-    ap = [0] * (m + 1)
-    for i in a:
-        op += 1
-        ap[i] += 1
-    for i in b:
-        op += 1
-        ap[i] += 1
-    for i in c:
-        op += 1
-        ap[i] += 1
-    for i in ap:
-        op += 1
-        if(i == 3):
-            rez = True
-    return rez
+for line in A:
+    buffer = 0
+    for i in line:
+        buffer += i**2
+    e_norm.append(sqrt(buffer))
 
-def merge():
-    global op
-    rez = False
-    i  = 0
-    j = 0
-    while(i != len(a)-1 and j != len(b)-1):
-        if(a[i] != b[j]):
-            pass
-    return rez
-
-
-print(n3(), op)
-op = 0
-print(n2(), op)
-op = 0
-print(maxnm(10), op)
-op = 0
-print(merge(), op)
+print(e_norm)
