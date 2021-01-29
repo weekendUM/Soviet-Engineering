7e0043732b6748cba9028c602e0a36523788e9b8
Schronk <damian.sirbu01@e-uvt>
2021-01-29 17:22:11 +0200

    find element in sorted matrix

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,48 +1,22 @@
-a = [5,3,7]
-b = [3,7,9]
+A = [
+    [3,6,10],
+    [5,9,12],
+    [11,14,16]
+    ]
 
-class number:
-    def __init__(self, arr = [0]):
-        self.arr = arr
+def searchEl(el):
+    i = 0
+    j = len(A[0]) - 1
+    while(i != len(A) and j != -1):
+        if(A[i][j] == el):
+            return True
+        elif(A[i][j] > el):
+            j -= 1
+            continue
+        elif(A[i][j] < el):
+            i += 1
+            continue
+        else: break
+    return False
 
-    def __gt__(self, other):
-        arr = self.arr
-        guest = other.arr
-        for i in range(len(arr) - 1):
-            if(arr[i] > guest[i]):
-                return 1
-            elif(arr[i] < guest[i]):
-                return -1
-
-        if (arr[-1] > guest[-1]):
-            return 1
-        return -1
-
-    def __add__(self, other):
-        res = []
-        carry = 0
-        arr = self.arr
-        guest = other.arr
-        for i in range(len(arr) - 1, -1, -1):
-            new = (arr[i] + guest[i] + carry) % 10
-            res.append(new)
-            carry = (arr[i] + guest[i] + carry) // 10
-        return number(reversed(res))
-
-    def __str__(self):
-        res = str()
-        for el in self.arr:
-            res += str(el)
-        return res
-
-    def __sub__(self, other):
-        for i in range(len(other.arr)):
-            other.arr[i] = -other.arr[i]
-        return self + other
-        
-
-n1 = number(a)
-n2 = number(b)
-print(n1 > n2)
-print(n1 + n2)
-print(n1 - n2)
+print(searchEl(3))
