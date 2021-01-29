90f7d457a3f3ed2f71b09a5d8892adbfe09d45cb
Schronk <damian.sirbu01@e-uvt>
2021-01-29 16:26:43 +0200

    finding element in rotated array with unknown k logn

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,4 +1,4 @@
-x = [1,3,4,6,7,8,10,12]
+x = [1,3,4,6,8,10,12]
 
 def rotate(k = 1):
     global x
@@ -26,12 +26,26 @@ def binarySearch(el, left, right):
 def findEl(el, breakpoint = 1):
     return binarySearch(el, 0, breakpoint - 1) or binarySearch(el, breakpoint, len(x)-1)
 
+def findBreakpoint(left, right):
+    if(right < left): return -1
+    if(left == right): return left
+    mid = (left + right) // 2
+    if(mid < right and x[mid + 1] < x[mid]):
+        return mid
+    if(mid > left and x[mid] < x[mid - 1]):
+        return mid - 1
+
+    if(x[left] > x[mid]):
+        return findBreakpoint(left, mid - 1)
+    return findBreakpoint(mid + 1, right)
+
+
 
 k = 3
 print(x)
 rotate(k)
 print(x)
-print(findEl(7, k))
+print(findEl(7, findBreakpoint(0, len(x) - 1) + 1))
 
 
 
