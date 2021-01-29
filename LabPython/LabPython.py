e868c19c742594c4cf11e54faa24631831d5e1ad
Schronk <damian.sirbu01@e-uvt>
2021-01-29 15:46:38 +0200

    rotating list by k elements

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,19 +1,16 @@
-a = [1, 3, 5, 11]
-b = [3, 7, 11, 13]
+x = [1,3,4,6,8,10,12]
 
-def greedy():
-    helper = [] #list of valid segments
-    for i in range(len(a)):
-        valid = True
-        for el in helper:
-            if(a[i] > el[0] and a[i] < el[1]):
-                valid = False
-                break
-        if(valid):
-            helper.append((a[i],b[i]))
-    print(helper)
+def rotate(k = 1):
+    global x
+    buffer = []
+    for i in range(len(x)-k,len(x)):
+        buffer.append(x[i])
+    for i in range(len(x)-k):
+        buffer.append(x[i])
+    
+    x = buffer
 
-greedy()
+print(x)
+rotate(3)
+print(x)
 
-#T(n)=nk where k is the number of valid segments up to a certain point
-#for many disjunct segments k will be very close to n
