0d937d3a1de07f15e63c242fd592b7959f9807a5
Schronk <damian.sirbu01@e-uvt>
2021-01-29 12:32:58 +0200

    ex 4 from example exam
    mentioning that i already have ex 5 with memoization in some revision

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,16 +1,19 @@
-x = [4,7,2,3,9,10,13,1]
+a = [1, 3, 5, 11]
+b = [3, 7, 11, 13]
 
+def greedy():
+    helper = [] #list of valid segments
+    for i in range(len(a)):
+        valid = True
+        for el in helper:
+            if(a[i] > el[0] and a[i] < el[1]):
+                valid = False
+                break
+        if(valid):
+            helper.append((a[i],b[i]))
+    print(helper)
 
-def alg(i,j):
-    if(i==j):
-        return x[i]
-    else:
-        m = (i+j)%2
-        a = alg(i,m)
-        b = alg(m+1,j)
-        return a * b
+greedy()
 
-try:
-    print(alg(0,len(x)-1))
-except Exception as e:
-    print(e)
+#T(n)=nk where k is the number of valid segments up to a certain point
+#for many disjunct segments k will be very close to n
