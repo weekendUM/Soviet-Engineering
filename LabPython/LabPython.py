9a4d5efe51386caac3029ab0b465a9491014170e
Schronk <damian.sirbu01@e-uvt>
2021-01-29 17:11:00 +0200

    operations with numbers stored in arrays

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,51 +1,48 @@
-x = [1,3,4,6,8,10,12]
+a = [5,3,7]
+b = [3,7,9]
 
-def rotate(k = 1):
-    global x
-    buffer = []
-    for i in range(len(x)-k,len(x)):
-        buffer.append(x[i])
-    for i in range(len(x)-k):
-        buffer.append(x[i])
-    
-    x = buffer
+class number:
+    def __init__(self, arr = [0]):
+        self.arr = arr
 
-def binarySearch(el, left, right):
-    if(left == right):
-        if(el == x[left]):
-            return True
-        return False
-    mid = (left + right) // 2
-    if(el == x[mid]):
-        return True
-    elif(el < x[mid]):
-        return binarySearch(el, left, mid - 1)
-    elif(el > x[mid]):
-        return binarySearch(el, mid + 1, right)
+    def __gt__(self, other):
+        arr = self.arr
+        guest = other.arr
+        for i in range(len(arr) - 1):
+            if(arr[i] > guest[i]):
+                return 1
+            elif(arr[i] < guest[i]):
+                return -1
 
-def findEl(el, breakpoint = 1):
-    return binarySearch(el, 0, breakpoint - 1) or binarySearch(el, breakpoint, len(x)-1)
+        if (arr[-1] > guest[-1]):
+            return 1
+        return -1
 
-def findBreakpoint(left, right):
-    if(right < left): return -1
-    if(left == right): return left
-    mid = (left + right) // 2
-    if(mid < right and x[mid + 1] < x[mid]):
-        return mid
-    if(mid > left and x[mid] < x[mid - 1]):
-        return mid - 1
+    def __add__(self, other):
+        res = []
+        carry = 0
+        arr = self.arr
+        guest = other.arr
+        for i in range(len(arr) - 1, -1, -1):
+            new = (arr[i] + guest[i] + carry) % 10
+            res.append(new)
+            carry = (arr[i] + guest[i] + carry) // 10
+        return number(reversed(res))
 
-    if(x[left] > x[mid]):
-        return findBreakpoint(left, mid - 1)
-    return findBreakpoint(mid + 1, right)
+    def __str__(self):
+        res = str()
+        for el in self.arr:
+            res += str(el)
+        return res
 
+    def __sub__(self, other):
+        for i in range(len(other.arr)):
+            other.arr[i] = -other.arr[i]
+        return self + other
+        
 
-
-k = 3
-print(x)
-rotate(k)
-print(x)
-print(findEl(7, findBreakpoint(0, len(x) - 1) + 1))
-
-
-
+n1 = number(a)
+n2 = number(b)
+print(n1 > n2)
+print(n1 + n2)
+print(n1 - n2)
