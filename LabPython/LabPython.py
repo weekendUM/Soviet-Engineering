62eb6b873d62572a1257496007ce8af40feaa4d6
Schronk <damian.sirbu01@e-uvt>
2021-01-29 11:53:04 +0200

    ex 2 from example exam

--- LabPython/LabPython.py
+++ LabPython/LabPython.py
@@ -1,5 +1,20 @@
-from binary import binary
+def poz(x : list, v):
+    if(v < x[0]):
+        return 0
+    elif(v > x[-1]):
+        return len(x)
+    else:
+        for i in range(len(x) - 1):
+            if(v < x[i+1] and v > x[i]):
+                return i+1
 
-a = binary([1,0,0,1,0,0,0])
-print(a)
-print(a.dec())
+def alg(x = [3,6,2,7,5]):
+    for i in range(1,len(x)):
+        a = x[i]
+        j = poz(x[:i], x[i])
+        for k in range(i-1, j-1, -1):
+            x[k+1] = x[k]
+        x[j]=a
+    return x
+
+print(alg())
